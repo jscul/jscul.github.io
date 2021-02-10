1d3f6b5acd6debb0d9cde938337b5fbe3321dbf7
jscul <cullen.scott.john@gmail.com>
2021-02-09 23:29:13 -0800

    Updates

--- src/game/connection/index.js
+++
@@ -1,6 +0,0 @@
-import ServerNetwork from '../server/network';
-import ClientNetwork from '../Client/network';
-
-// callback is emit state from server (texture dependencies)
-const server = new ServerNetwork();
-const client = new ClientNetwork();
